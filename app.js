d7a64076faaea1dcea9d9f17711aa1125201f968
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-04-15 16:13:41 +0530

    Setup :construction_worker:

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const expressip = require('express-ip');
 const path = require('path');
 const handlebars = require('express3-handlebars');
 
@@ -9,7 +10,7 @@ app.engine('.hbs', handlebars({ extname: '.hbs' }));
 
 app.set("PORT", PORT);
 
-// app.use(expressip().getIpInfoMiddleware);
+app.use(expressip().getIpInfoMiddleware);
 
 app.use(express.static(path.join(__dirname, 'assets')));
 app.set('views', path.join(__dirname, 'views'));
